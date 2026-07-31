163393e6ee0a6d1a641c7866cfde2f88b5d32ffb
SamiLhll <elhilali.sami@gmail.com>
2026-07-31 16:36:08 +0200

    remove unused commands

--- server.R
+++ server.R
@@ -15,41 +15,15 @@ library(gridExtra)
 
 ### Load data ----
 
-sc1conf = readRDS("data/SingleCell/APO_SYM_V1/sc1conf.rds")
-sc1def  = readRDS("data/SingleCell/APO_SYM_V1/sc1def.rds")
-sc1gene = readRDS("data/SingleCell/APO_SYM_V1/sc1gene.rds")
-sc1meta = readRDS("data/SingleCell/APO_SYM_V1/sc1meta.rds")
 sym_Atlas = readRDS("data/SymAtlas/Sym_clustering.dims_50.res_05.RDS")
 
 
-### Load helpers ----
-source("helpers/shinycell_helpers.R",local = T)
+
  
 ### Start server code ----
 shinyServer(function(input, output, session) { 
   ### For all tags and Server-side selectize 
   observe_helpers() 
-  optCrt="{ option_create: function(data,escape) {return('<div class=\"create\"><strong>' + '</strong></div>');} }" 
-  updateSelectizeInput(session, "sc1a1inp2", choices = names(sc1gene), server = TRUE, 
-                       selected = sc1def$gene1, options = list( 
-                         maxOptions = 7, create = TRUE, persist = TRUE, render = I(optCrt))) 
-  updateSelectizeInput(session, "sc1a3inp1", choices = names(sc1gene), server = TRUE, 
-                       selected = sc1def$gene1, options = list( 
-                         maxOptions = 20, create = TRUE, persist = TRUE, render = I(optCrt))) 
-  updateSelectizeInput(session, "sc1a3inp2", choices = names(sc1gene), server = TRUE, 
-                       selected = sc1def$gene2, options = list( 
-                         maxOptions = 7, create = TRUE, persist = TRUE, render = I(optCrt))) 
-  updateSelectizeInput(session, "sc1b2inp1", choices = names(sc1gene), server = TRUE, 
-                       selected = sc1def$gene1, options = list( 
-                         maxOptions = 7, create = TRUE, persist = TRUE, render = I(optCrt))) 
-  updateSelectizeInput(session, "sc1b2inp2", choices = names(sc1gene), server = TRUE, 
-                       selected = sc1def$gene2, options = list( 
-                         maxOptions = 7, create = TRUE, persist = TRUE, render = I(optCrt))) 
-  updateSelectizeInput(session, "sc1c1inp2", server = TRUE, 
-                       choices = c(sc1conf[is.na(fID)]$UI,names(sc1gene)), 
-                       selected = sc1conf[is.na(fID)]$UI[1], options = list( 
-                         maxOptions = length(sc1conf[is.na(fID)]$UI) + 3, 
-                         create = TRUE, persist = TRUE, render = I(optCrt))) 
   
   ### CODE FOR TABS
   
@@ -71,8 +45,6 @@ shinyServer(function(input, output, session) {
   ### LOPIT
   source(file = "Server_sources/LOPIT.sources.R",local=TRUE)
   
-  ### Old Shinycell tabs
-  source(file = "Server_sources/Old_shinycell.sources.R",local=TRUE)
 
 # ShinyServer ending bracket     
 }) 
--- ui.R
+++ ui.R
@@ -18,9 +18,6 @@ library(ggplot2)
 library(hdf5r)
 library(ggdendro)
 
-### load data ----
-sc1conf = readRDS("data/SingleCell/APO_SYM_V1/sc1conf.rds")
-sc1def  = readRDS("data/SingleCell/APO_SYM_V1/sc1def.rds")
 
 ### Start server code ----
 shinyUI(fluidPage(
@@ -30,23 +27,13 @@ shinyUI(fluidPage(
   tags$head(tags$style(HTML(".shiny-output-error-validation {color: red; font-weight: bold;}"))), 
   list(tags$style(HTML(".navbar-default .navbar-nav { font-weight: bold; font-size: 20px; }"))), 
   
-  ### Page title 
-  # titlePanel(h1("Aiptasia-db")),
-  # p("The web-application to exploit the public resources of the sea anemone Aiptasia"),
-  # hr(),
   ###########################################################
   # HEADER
   ###########################################################
   title = "Aiptasia-db",
   titlePanel(title= div(img(src = "img/Aiptasia-db_logo_2025.png",style="width: 300px"))),
-  #     
-  # hr(),
-  
   
   tabsetPanel( 
-    # id = NULL,
-    # widths = c(2,10),
-    
     
     
     #######################################################################
@@ -54,7 +41,6 @@ shinyUI(fluidPage(
     #######################################################################
     
     tabPanel(title =  "Home",
-    # div(img(src = "img/Aiptasia_zoom.png",style="width: 80px")),
              source("UI_sources/Tab_Home.R",local = TRUE)[1]
     ),
     
@@ -65,9 +51,6 @@ shinyUI(fluidPage(
     tabPanel(title = "Fun. annotation",
              source("UI_sources/Tab_FunAnnot_refseq_Aip1.1.R",local = TRUE)[1]
     ),
-    # tabPanel(title = "XP <-> AIP",
-    #          source("UI_sources/Tab_FunAnnot_XP_AIP.R",local = TRUE)[1]
-    # ),
     
     ######################################################################
     # Public data
@@ -77,48 +60,10 @@ shinyUI(fluidPage(
              source("UI_sources/Tab_NCBI_public_data.R",local = TRUE)[1]
     ),
     
-    
     tabPanel(title = "Sym Atlas",
              source("UI_sources/Tab_Sym_atlas.R", local = TRUE)[1]
     ),
-    ######################################################################
-    # Single-Cell Atlas
-    ######################################################################
-    
-    ### Apo Sym
-    # tabPanel(title = "scAtlas Browser",
-    #          tabsetPanel(type = "tabs",
-    #                      ### TAB SINGLE CELL 1: cellInfo 
-    #                      tabPanel(title = HTML("Cell<br/>Information"),
-    #                               source("UI_sources/Tab_SingleCell_CellInfo.R",local = TRUE)[1]
-    #                      ),     # End of tab (2 space) 
-    #                      
-    #                      
-    #                      ### TAB SINGLE CELL 2 : Gene Expression 
-    #                      tabPanel(title = HTML("Gene<br/>Expression"),
-    #                               source("UI_sources/Tab_SingleCell_GeneExpr.R",local = TRUE)[1]
-    #                      ),
-    #                      tabPanel(title = HTML("Gene<br/>Coexpression"),
-    #                               source("UI_sources/Tab_SingleCell_CoExpr.R",local = TRUE)[1]
-    #                               
-    #                      ), 
-    #                      
-    #                      ### Tab1.c1: violinplot / boxplot 
-    #                      tabPanel(title = HTML("Boxplot<br/>"),  
-    #                               source("UI_sources/Tab_SingleCell_Violin.R",local = TRUE)[1]
-    #                      ), 
-    #                      
-    #                      ### Tab1.c2: Proportion plot 
-    #                      tabPanel(title = HTML("Cell<br/>Proportions"),
-    #                               source("UI_sources/Tab_SingleCell_Prop.R",local = TRUE)[1]
-    #                               
-    #                      ),     # End of tab (2 space) 
-    #                      
-    #                      ### Tab1.d1: Multiple gene expr 
-    #                      tabPanel(title = HTML("Heatmap"), 
-    #                               source("UI_sources/Tab_SingleCell_Bubble.R", local = TRUE)[1]
-    #                               
-    #                      ))),
+
     tabPanel(title = "LOPIT",
              source("UI_sources/Tab_LOPIT.R",local = TRUE)[1]
     ),
@@ -134,8 +79,8 @@ shinyUI(fluidPage(
   # FOOTER
   ##################################################
   hr(),
-  # h2("About"),
-  div("Your seeing Aiptasia-db v1.2.4 (updated on 2025/11/11) It is a project from the ",
+
+  div("Your seeing Aiptasia-db v1.2.5 (updated on 2026/07/31) It is a project from the ",
       a("Guse lab", href = "https://guselab.de/",target="_blank"),
       " in Ludwig-Maximillians Universität."),
   div("It is implemented and maintained by ",
