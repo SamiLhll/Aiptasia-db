6b83d834c82b06fa205aa0c27d6b8977ccb90c0b
SamiLhll <elhilali.sami@gmail.com>
2026-08-03 17:31:42 +0200

    improved sym atlas tab

--- UI_sources/Tab_Sym_atlas.R
+++ UI_sources/Tab_Sym_atlas.R
@@ -1,29 +1,67 @@
-list(
-  h5("Browse the Single cell atlas of Symbiotic polyps."),
-  p("Scroll down to query :"),
-  p("1 - The expression of a single gene (FeaturePlot)"),
-  p("2 - The expression of a list of genes (Dotplot)"),
-  p("3 - The full table of marker genes"),
-  hr(),
-  img(src = "img/Symbiotic_atlas_dimplot_screenshot.png",style="width: 300px"),
-  hr(),
-  h4("1 - Query the expression of a single gene (FeaturePlot)"),
-  fluidRow(column(3,list(textInput("query_feature_symAtlas", "Enter a LOC gene ID", ""),
-                         actionButton("plot_featureplot_symAtlas_button", "plot"))),
-           column(7,list(plotOutput("FeaturePlot_SymAtlas"),
-                         dataTableOutput("subset_fun_annot_feature"),
-                         dataTableOutput("subset_marker_feature")))),
-  # p("annotation for this gene (Fun annotation and expression)"),
-  fluidRow(),
-  hr(),
-  h4("2 - Query the expression of a list of genes (DotPlot)"),
-  fluidRow(column(3,list(textInput("query_list_of_gene_ids_sym_atlas", "Enter a list of \",\" separated IDs", ""),
-                         actionButton("search_button_list_of_features_sym_atlas", "plot"))),
-           column(7,list(plotOutput("DotPlot_SymAtlas"),
-                         dataTableOutput("subset_fun_annot_feature_list"),
-                         dataTableOutput("subset_marker_feature_list")))),
-  hr(),
-  h4("3 - Query the table of marker genes by keyword"),
-  # uiOutput("column_selector"),
-  dataTableOutput("table_markers_Sym_atlas")
+navset_pill_list(
+  widths = c(2, 10),
+  
+  ################################################
+  # OVERVIEW / HOME PANEL
+  ################################################
+  nav_panel(
+    title = "Overview",
+    h5("Browse the Single cell atlas of Symbiotic polyps."),
+    p("Use the panels on the left to query:"),
+    p("1 - The expression of a single gene (FeaturePlot)"),
+    p("2 - The expression of a list of genes (Dotplot)"),
+    p("3 - The full table of marker genes"),br(),
+    div(style = "text-align: left;",
+        img(src = "img/Symbiotic_atlas_dimplot_screenshot.png", style = "width: 100%; max-width: 400px;")
+    )
+    
+    
+  ),
+  
+  ################################################
+  # QUERY 1 - SINGLE GENE
+  ################################################
+  nav_panel(
+    title = "1 - Single gene (Featureplot)",
+    h4("Query the expression of a single gene (FeaturePlot)"),
+    fluidRow(
+      column(3, list(
+        textInput("query_feature_symAtlas", "Enter a LOC gene ID", ""),
+        actionButton("plot_featureplot_symAtlas_button", "plot")
+      )),
+      column(9, list(
+        plotOutput("FeaturePlot_SymAtlas"),
+        dataTableOutput("subset_fun_annot_feature"),
+        dataTableOutput("subset_marker_feature")
+      ))
+    )
+  ),
+  
+  ################################################
+  # QUERY 2 - LIST OF GENES
+  ################################################
+  nav_panel(
+    title = "2 - Gene list (Dotplot)",
+    h4("Query the expression of a list of genes (DotPlot)"),
+    fluidRow(
+      column(3, list(
+        textInput("query_list_of_gene_ids_sym_atlas", "Enter a list of \",\" separated IDs", ""),
+        actionButton("search_button_list_of_features_sym_atlas", "plot")
+      )),
+      column(9, list(
+        plotOutput("DotPlot_SymAtlas"),
+        dataTableOutput("subset_fun_annot_feature_list"),
+        dataTableOutput("subset_marker_feature_list")
+      ))
+    )
+  ),
+  
+  ################################################
+  # QUERY 3 - MARKER TABLE
+  ################################################
+  nav_panel(
+    title = "3 - Table of marker genes",
+    h4("Query the table of marker genes by keyword"),
+    dataTableOutput("table_markers_Sym_atlas")
+  )
 )
--- ui.R
+++ ui.R
@@ -2,11 +2,10 @@
 # Aiptasia-db - v.1.2.2
 # Sami El Hilali
 ##############################################################################
-
 ### Load libraries ----
 library(shiny) 
 library(shinyhelper) 
-library(shinythemes)
+library(bslib)
 library(data.table) 
 library(Matrix) 
 library(DT) 
@@ -18,78 +17,78 @@ library(ggplot2)
 library(hdf5r)
 library(ggdendro)
 
-
 ### Start server code ----
-shinyUI(fluidPage(
-  # theme = shinytheme("cosmo"),
-  # theme = "bootstrap.min.css",
-  ### HTML formatting of error messages 
-  tags$head(tags$style(HTML(".shiny-output-error-validation {color: red; font-weight: bold;}"))), 
-  list(tags$style(HTML(".navbar-default .navbar-nav { font-weight: bold; font-size: 20px; }"))), 
-  
-  ###########################################################
-  # HEADER
-  ###########################################################
-  title = "Aiptasia-db",
-  titlePanel(title= div(img(src = "img/Aiptasia-db_logo_2025.png",style="width: 300px"))),
-  
-  tabsetPanel( 
+
+shinyUI(
+  page_navbar(
+    fillable = FALSE,
+    title = div(
+      img(src = "img/Aiptasia-db_logo_2025_nobg.png", style = "width: 500px; margin-right: 50px;")),
+    window_title = "Aiptasia-db",
+    theme = bs_theme(
+      version = 5,
+      bootswatch = "minty",
+      secondary = "#8aa6a3",
+      base_font = font_google("Inter"),
+      heading_font = font_google("Inter", wght = 600)
+    ),
     
+    ### HTML formatting of error messages
+    header = tags$head(
+      tags$style(HTML("
+        .shiny-output-error-validation {color: red; font-weight: bold;}
+        .navbar .nav-link { font-weight: bold; font-size: 20px; }
+      "))
+    ),
     
     #######################################################################
     # HOME
     #######################################################################
-    
-    tabPanel(title =  "Home",
-             source("UI_sources/Tab_Home.R",local = TRUE)[1]
+    nav_panel(title = "Home",
+              source("UI_sources/Tab_Home.R", local = TRUE)[1]
     ),
     
     ######################################################################
     # Functional annotation
     ######################################################################
-    
-    tabPanel(title = "Fun. annotation",
-             source("UI_sources/Tab_FunAnnot_refseq_Aip1.1.R",local = TRUE)[1]
+    nav_panel(title = "Fun. annotation",
+              source("UI_sources/Tab_FunAnnot_refseq_Aip1.1.R", local = TRUE)[1]
     ),
     
     ######################################################################
     # Public data
     ######################################################################
-    
-    tabPanel(title = "NCBI datasets",
-             source("UI_sources/Tab_NCBI_public_data.R",local = TRUE)[1]
+    nav_panel(title = "NCBI datasets",
+              source("UI_sources/Tab_NCBI_public_data.R", local = TRUE)[1]
     ),
     
-    tabPanel(title = "Sym Atlas",
-             source("UI_sources/Tab_Sym_atlas.R", local = TRUE)[1]
+    nav_panel(title = "Sym Atlas",
+              source("UI_sources/Tab_Sym_atlas.R", local = TRUE)[1]
     ),
-
-    tabPanel(title = "LOPIT",
-             source("UI_sources/Tab_LOPIT.R",local = TRUE)[1]
+    nav_panel(title = "LOPIT",
+              source("UI_sources/Tab_LOPIT.R", local = TRUE)[1]
     ),
-    tabPanel(title = "Downloads",
-             source("UI_sources/Tab_Downloads.R",local = TRUE)[1]
-             ),
-    tabPanel(title = "Symbionts",
-             source("UI_sources/Tab_Symbionts.R",local = TRUE)[1]
-             )
+    nav_panel(title = "Downloads",
+              source("UI_sources/Tab_Downloads.R", local = TRUE)[1]
+    ),
+    nav_panel(title = "Symbionts",
+              source("UI_sources/Tab_Symbionts.R", local = TRUE)[1]
+    ),
     
-  ),
-  ##################################################
-  # FOOTER
-  ##################################################
-  hr(),
-
-  div("Your seeing Aiptasia-db v1.2.5 (updated on 2026/07/31) It is a project from the ",
-      a("Guse lab", href = "https://guselab.de/",target="_blank"),
-      " in Ludwig-Maximillians Universität."),
-  div("It is implemented and maintained by ",
-      a("Sami El Hilali", href = "mailto:elhilali.sami@gmail.com",target="_blank")),
-  br(),
-  div(column(img(src = "img/Logo_LMU.png",style="height: 60px"),width = 2),
-      "",
-      img(src = "img/Logo_guselab.jpeg",style="height: 65px"))
-))
-
-
-
+    ##################################################
+    # FOOTER
+    ##################################################
+    footer = div(
+      style = "margin-top: 60px; padding: 20px 40px; border-top: 1px solid #dee2e6; background-color: #f8f9fa;",
+      p("You're seeing Aiptasia-db v1.2.5 (updated on 2026/07/31). It is a project from the ",
+        a("Guse lab", href = "https://guselab.de/", target = "_blank"),
+        " in Ludwig-Maximilians-Universität."),
+      p("It is implemented and maintained by ",
+        a("Sami El Hilali", href = "mailto:elhilali.sami@gmail.com", target = "_blank")),
+      div(style = "display: flex; align-items: center; gap: 15px; margin-top: 15px;",
+          img(src = "img/Logo_LMU.png", style = "height: 50px"),
+          img(src = "img/Logo_guselab.jpeg", style = "height: 55px")
+      )
+    )
+  )
+)
